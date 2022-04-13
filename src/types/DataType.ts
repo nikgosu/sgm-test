7b49719a88ee039129ae6a3ade62902e04d340e2
NIKGOSU\GosuJungler <gosujungler@gmail.com>
2022-04-13 23:09:21 +0300

    Refactor interfaces in DataTypes

--- src/types/DataType.ts
+++ src/types/DataType.ts
@@ -1,28 +1,28 @@
-export interface DataLetter {
+export interface Letter {
   value: number | boolean
   dateRelease?: string
 }
 
-export interface DataYear {
-  XX: DataLetter,
-  YY: DataLetter,
-  ZZ: DataLetter,
+export interface Year {
+  XX: Letter,
+  YY: Letter,
+  ZZ: Letter,
 }
 
-export interface DataRegion {
-  2017: DataYear,
-  2018?: DataYear,
-  2019?: DataYear,
+export interface Region {
+  2017: Year,
+  2018?: Year,
+  2019?: Year,
 }
 
-export interface DataG {
-  G: DataRegion
+export interface G {
+  G: Region
 }
 
 export interface Data {
-  Kyivska: DataG,
-  Odeska: DataG,
-  Lvivska: DataG,
+  Kyivska: G,
+  Odeska: G,
+  Lvivska: G,
 }
 
 
